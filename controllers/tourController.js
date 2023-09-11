7c41521158c0c64d5c9367e582514c40cc4676d9
mzakiullahusman <zaki.ullah@codistan.org>
2023-09-11 16:59:53 +0500

    Update: Added basic filtering for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,18 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // Execute query
+    const tours = await query;
+
+    // Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,

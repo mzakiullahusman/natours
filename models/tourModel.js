c71120449b80689ea1b337ba2385868722fd3399
mzakiullahusman <zaki.ullah@codistan.org>
2023-09-13 17:02:15 +0500

    Update: Validations added from mongo and validator library

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,15 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must be less than or equal to 40 characters',
+      ],
+      minlength: [
+        7,
+        'A tour name must be greater than or equal to 7 characters',
+      ],
+      validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -21,11 +31,29 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficluty can be easy, medium or difficult only',
+      },
     },
-    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [0, 'Rating must be above 0'],
+      max: [5, 'Rating must be equal to or below 5'],
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // Only for new documents, does not work for update
+          return val < this.price;
+        },
+        message: 'Discount price, ({VALUE}) must be less than regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -63,7 +91,7 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// Document middleware: runs before .save() and .create()
+// Document middleware: runs before .save() and .create(), does not work for update
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
